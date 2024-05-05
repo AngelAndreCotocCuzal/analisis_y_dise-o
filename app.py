3e6a00414cde97729bfd56c42ec76a320ea28f38
Angel André Cotoc Cuzal <78944937+AngelAndreCotocCuzal@users.noreply.github.com>
2024-05-05 16:11:10 -0600

    Cambidos

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 # from View.ventana_login import Main_login
-from view.ventana_login import	Main_login
-
+from view.ventana_login import Main_login
+# Comentario
 
 app = QApplication(sys.argv)
 window = Main_login()
